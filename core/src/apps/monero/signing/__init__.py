672dcd23d3795e6358bc3c160a410c8c6874f1a9
Dusan Klinec <dusan.klinec@gmail.com>
2020-04-09 17:16:48 +0200

    xmr: major protocol upgrade, CLSAG support added
      - CLSAG signature scheme added
      - type hints added
    
    xmr: optimize protocol, send only required data
      - real_out_additional_tx_keys contains only one element as nothing more is needed during signature
      - only src_entr.outputs[index] is HMACed and always present. Other outputs are present only if needed which reduces comm and CPU overhead.
      - getting rid of subaddresses dictionary (memory requirements), now subaddr indices are present per source entry so keys are computed when needed
    
    xmr: prepare for permutation sending removal, specify index
      - specify source entry ordering index prior sorting by key images as original HMAC keys are generated based on these.
      - permutation checked just by valid HMACs, size of the set, key image sort order
      - sending permutation is now deprecated, will be removed in the following protocol versions
      - more strict state transition checks, guard strict check with respect to steps ordering

--- core/src/apps/monero/signing/__init__.py
+++ core/src/apps/monero/signing/__init__.py
@@ -18,39 +18,9 @@ class RctType:
     There are two types of monero Ring Confidential Transactions:
     1. RCTTypeFull = 1 (used if num_inputs == 1)
     2. RCTTypeSimple = 2 (for num_inputs > 1)
+    3. Bulletproof2 = 4, HF11+
 
     There is actually also RCTTypeNull but we ignore that one.
     """
 
-    Full = 1
-    Simple = 2
-
-
-class RsigType:
-    """
-    Range signature types
-
-    There are four types of range proofs/signatures in official Monero:
-    1. RangeProofBorromean = 0
-    2. RangeProofBulletproof = 1
-    3. RangeProofMultiOutputBulletproof = 2
-    4. RangeProofPaddedBulletproof = 3
-
-    We simplify all the bulletproofs into one.
-    """
-
-    Borromean = 0
-    Bulletproof = 1
-
-
-def get_monero_rct_type(bp_version=1):
-    """
-    Returns transaction RctType according to the BP version.
-    Only HP9+ is supported, thus only Simple variant is concerned.
-    """
-    if bp_version == 1:
-        return 3  # TxRctType.Bulletproof
-    elif bp_version == 2:
-        return 4  # TxRctType.Bulletproof2
-    else:
-        raise ValueError("Unsupported BP version")
+    Bulletproof2 = 4
