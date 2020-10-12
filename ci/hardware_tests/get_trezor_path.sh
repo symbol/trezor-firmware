ba69f87f54ecac0a25129cc496524d5015bb21ae
Tomas Susanka <tsusanka@gmail.com>
2020-10-12 17:02:19 +0200

    ci: update hardware ports

--- ci/hardware_tests/get_trezor_path.sh
+++ ci/hardware_tests/get_trezor_path.sh
@@ -6,4 +6,4 @@ if [ $# -ne 1 ]
     exit 1
 fi
 
-nix-shell --run "pipenv run trezorctl list | grep '$1' | cut -d' ' -f1 | tr -d '\n'"
+nix-shell --run "poetry run trezorctl list | grep '$1' | cut -d' ' -f1 | tr -d '\n'"
