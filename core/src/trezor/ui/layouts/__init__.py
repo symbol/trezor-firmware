c1fb285d8f22412f35433f1d502bd9ff78842d35
Martin Milata <martin@martinmilata.cz>
2021-04-12 17:03:18 +0200

    make t1 boot to the point usb is initialized

--- core/src/trezor/ui/layouts/__init__.py
+++ core/src/trezor/ui/layouts/__init__.py
@@ -4,9 +4,4 @@ from .common import *  # noqa: F401,F403
 
 # NOTE: using any import magic probably causes mypy not to check equivalence of
 #       layout type signatures across models
-if utils.MODEL == "1":
-    from .t1 import *  # noqa: F401,F403
-elif utils.MODEL == "T":
-    from .tt import *  # noqa: F401,F403
-else:
-    raise ValueError("Unknown Trezor model")
+from .tt import *  # noqa: F401,F403
