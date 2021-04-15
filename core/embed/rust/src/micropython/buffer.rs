d5a05ecde139af91872b10b87a5754a133a99d2e
Jan Pochyla <jpochyla@gmail.com>
2021-04-15 16:15:13 +0200

    core: Improve docs for micropython::Buffer

--- core/embed/rust/src/micropython/buffer.rs
+++ core/embed/rust/src/micropython/buffer.rs
@@ -4,11 +4,16 @@ use crate::{error::Error, micropython::obj::Obj};
 
 use super::ffi;
 
-/// Represents a slice of bytes stored on the MicroPython heap and owned by
-/// values that obey the buffer protocol, such as `bytes`, `str`, `bytearray` or
-/// `memoryview`.
+/// Represents an immutable slice of bytes stored on the MicroPython heap and
+/// owned by values that obey the buffer protocol, such as `bytes`, `str`,
+/// `bytearray` or `memoryview`.
+///
+/// # Safety
+///
+/// In most cases, it is unsound to store `Buffer` values in a GC-unreachable
+/// location, such as static data.
 pub struct Buffer {
-    ptr: *mut u8,
+    ptr: *const u8,
     len: usize,
 }
 
@@ -21,6 +26,11 @@ impl TryFrom<Obj> for Buffer {
             len: 0,
             typecode: 0,
         };
+        // SAFETY: We assume that if `ffi::mp_get_buffer` returns successfully,
+        // `bufinfo.buf` contains a pointer to data of `bufinfo.len` bytes. Here
+        // we consider this data either GC-allocated or effectively 'static, and
+        // store the pointer directly in `Buffer`. It is unsound to store
+        // `Buffer` values in a GC-unreachable location, such as static data.
         if unsafe { ffi::mp_get_buffer(obj, &mut bufinfo, ffi::MP_BUFFER_READ as _) } {
             Ok(Self {
                 ptr: bufinfo.buf as _,
@@ -43,12 +53,13 @@ impl Deref for Buffer {
 impl AsRef<[u8]> for Buffer {
     fn as_ref(&self) -> &[u8] {
         if self.ptr.is_null() {
-            // `ptr` can be null if len == 0.
+            // `self.ptr` can be null if len == 0.
             &[]
         } else {
-            // SAFETY: We assume that `ptr` is pointing to memory:
-            //  - immutable for the whole lifetime of immutable ref of `self`.
-            //  - of length `len` bytes.
+            // SAFETY: We assume that `self.ptr` is pointing to memory:
+            //  - without any mutable references,
+            //  - immutable for the whole lifetime of `&self`,
+            //  - with at least `self.len` bytes.
             unsafe { slice::from_raw_parts(self.ptr, self.len) }
         }
     }
