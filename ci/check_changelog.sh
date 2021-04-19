b7427fa99f8f079eacfc0262270adf832be2aec6
Martin Milata <martin@martinmilata.cz>
2021-04-19 13:32:52 +0200

    docs(core): use towncrier for generating CHANGELOG.md

---
+++ ci/check_changelog.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+base_branch=master
+fail=0
+subdirs="core python legacy/firmware legacy/bootloader"
+
+git fetch origin "$base_branch"
+
+for subdir in $subdirs
+do
+    echo "Checking $subdir"
+    files=$(git diff --name-only "origin/$base_branch..." -- "$subdir")
+
+    if echo "$files" | grep -Fq -v .changelog.d; then
+        if ! echo "$files" | grep -Fq .changelog.d; then
+            fail=1
+            echo "FAILURE! No changelog entry for changes in $subdir."
+        fi
+    fi
+done
+
+if [[ "$fail" -ne 0 ]]; then
+    echo "Please see https://docs.trezor.io/trezor-firmware/misc/changelog.html for instructions."
+fi
+
+exit $fail
